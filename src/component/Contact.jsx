839855dfb7dfdc24f82dc0c6eaf45eed479f9c60
nidhi <nidhigowdathelabailu2@gmail.com>
2024-04-01 17:55:48 +0530

    this is the minor change in the contact.jsx in responsiveness

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -11,20 +11,20 @@ export const Contact = () => {
           Contact <span className="text-cyan-600">Me</span>
         </h3>
         <p className="text-gray-400 mt-3 text-lg">Get In Touch</p>
-        <div className="mt-16 md:p-6 p-2 flex md:flex-row flex-col gap-6 max-w-5xl bg-gray-800 rounded-lg mx-auto">
+        <div className="mt-16 md:p-6 p-2 flex md:flex-row flex-col overflow-auto gap-6 max-w-5xl bg-gray-800 rounded-lg mx-auto">
           <form className="flex flex-col flex-1 gap-5" action="">
             <input type="text" placeholder="Your Name" />
             <input type="text" placeholder="Your Email Address"/>
             <textarea rows={10} placeholder="Your Message"></textarea>
             <button className="btn-primary w-fit">Send Message</button>
           </form>
-          <div className="flex  flex-col gap-7">
+          <div className="flex  flex-col gap-7 ">
             {data.map((val,i)=>(
               <div id={i} className="flex gap-4 w-fit items-center">
               <div className="min-w-[3.5rem] min-h-[3.5rem] text-3xl flex items-center justify-center text-white bg-cyan-600 rounded-full">
                 <ion-icon name={val.logo}></ion-icon>
               </div>
-              <p className="text-base">{val.text}</p>
+              <p className="text-xs sm:text-lg ">{val.text}</p>
             </div>
             ))}
           </div>
